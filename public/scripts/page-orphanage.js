bb01ec287a04dcf170869573c9177805e6cd987f
Andreia de Moraes <andreiadmrs@gmail.com>
2020-10-15 15:17:32 -0300

    Quarto dia. Começando a criar o backend e a adicionar dados dinamicamente.

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -15,7 +15,7 @@ osm.addTo(mymap);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -7,20 +7,35 @@ osm.addTo(mymap);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 });
 
-//create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMarker({ id, name, lat, lng } = orphanage) {
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
 
-//create and add marker
-L.marker([-20.3460456, -40.3140736], { icon }).addTo(mymap)
-    .bindPopup(popup)
+    L
+        .marker([lat, lng], { icon })
+        .addTo(mymap)
+        .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+
+    addMarker(orphanage)
+})
